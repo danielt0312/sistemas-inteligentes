fcd49637fd98802cec254b0996e5c5f3cd849390
Daniel Torres <2130072@upv.edu.mx>
2024-06-18 09:17:06 -0600

    porcentajes

--- U2/iti-271229_u2_torres_colorado_juan_daniel/classifier.py
+++ U2/iti-271229_u2_torres_colorado_juan_daniel/classifier.py
@@ -27,7 +27,7 @@ class Classifier():
 
         self.train_data, self.test_data, self.train_targets, self.test_targets  = train_test_split(self.data, self.targets, train_size=0.8, test_size=0.2, random_state=42)
 
-        self.vectorizer = CountVectorizer(stop_words=list(ENGLISH_STOP_WORDS))
+        self.vectorizer = CountVectorizer(stop_words=list(ENGLISH_STOP_WORDS) + ['de', 'la', 'que', 'el', 'en', 'y', 'a', 'los', 'del', 'se', 'las', 'por', 'un', 'para', 'con', 'no', 'una', 'su', 'al', 'es', 'lo', 'como', 'más', 'pero', 'sus', 'le', 'ya', 'o', 'sí', 'porque', 'esta', 'entre', 'cuando', 'muy', 'sin', 'sobre', 'también', 'me', 'hasta', 'hay', 'donde', 'quien', 'desde', 'todo', 'nos', 'durante', 'todos', 'uno', 'les', 'ni', 'contra', 'otros', 'ese', 'eso', 'ante', 'ellos', 'e', 'esto', 'mí', 'antes', 'algunos', 'qué', 'unos', 'yo', 'otro', 'otras', 'otra', 'él', 'tanto', 'esa', 'estos', 'mucho', 'quienes', 'nada', 'muchos', 'cual', 'poco', 'ella', 'estar', 'estas', 'algunas', 'algo', 'nosotros', 'mi', 'mis', 'tú', 'te', 'ti', 'tu', 'tus', 'ellas', 'nosotras', 'vosostros', 'vosostras', 'os', 'mío', 'mía', 'míos', 'mías', 'tuyo', 'tuya', 'tuyos', 'tuyas', 'suyo', 'suya', 'suyos', 'suyas', 'nuestro', 'nuestra', 'nuestros', 'nuestras', 'vuestro', 'vuestra', 'vuestros', 'vuestras', 'esos', 'esas', 'estoy', 'estás', 'está', 'estamos', 'estáis', 'están', 'esté', 'estés', 'esté', 'estemos', 'estéis', 'estén', 'estaré', 'estarás', 'estará', 'estaremos', 'estaréis', 'estarán', 'estaría', 'estarías', 'estaría', 'estaríamos', 'estaríais', 'estarían', 'estaba', 'estabas', 'estaba', 'estábamos', 'estabais', 'estaban', 'estuve', 'estuviste', 'estuvo', 'estuvimos', 'estuvisteis', 'estuvieron', 'estuviera', 'estuvieras', 'estuviera', 'estuviéramos', 'estuvierais', 'estuvieran', 'estuviese', 'estuvieses', 'estuviese', 'estuviésemos', 'estuvieseis', 'estuviesen', 'estando', 'estado', 'estada', 'estados', 'estadas', 'estad'])
         self.vectors = self.vectorizer.fit_transform(self.train_data)
 
     def load_data_targets(self, fname):
@@ -40,17 +40,13 @@ class Classifier():
     def classify(self, file):
         paragraphs = self.text2paragraphs(file)
         if not paragraphs:
-            print(f"No se encontraron parrafos en el archivo '{file}'. *** Omitiendo ***")
+            print(f"No valid paragraphs in file: {file}")
             return
-
         vtest = self.vectorizer.transform(paragraphs)
-        if vtest.shape[0] == 0:
-            print(f"El archivo '{file}' no tiene parrafos validos para la clasificacion. *** Omitiendo ***")
-            return
-
         prediction = self.classifier.predict(vtest)[0]
-        print(f"'{file}' : ", self.labels[prediction])
-
+        probabilities = self.classifier.predict_proba(vtest)[0]
+        labels_prob = ", ".join([f"{label}: {prob:.2%}" for label, prob in zip(self.labels, probabilities)])
+        print(f"{self.labels[prediction]} [{labels_prob}]\t| '{file}'")
 
     def loadLabels(self, files):
         self.labels = {fname[:2] for fname in files if fname.endswith(".txt")}
